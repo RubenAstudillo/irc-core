a4b24c980f3642a55d54933767c92f4a88eeb0ea
Ruben Astudillo <ruben.astud@gmail.com>
2019-01-27 11:03:38 -0300

    Bug on cabal new-build

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ mkDerivation, async, attoparsec, base, base64-bytestring
+, bytestring, Cabal, config-schema, config-value, containers
+, directory, filepath, free, gitrev, hashable, hookup, HsOpenSSL
+, HUnit, irc-core, kan-extensions, lens, network, process, psqueues
+, regex-tdfa, semigroupoids, split, stdenv, stm, template-haskell
+, text, time, transformers, unix, unordered-containers, vector, vty
+}:
+mkDerivation {
+  pname = "glirc";
+  version = "2.29";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  setupHaskellDepends = [ base Cabal filepath ];
+  libraryHaskellDepends = [
+    async attoparsec base base64-bytestring bytestring config-schema
+    config-value containers directory filepath free gitrev hashable
+    hookup HsOpenSSL irc-core kan-extensions lens network process
+    psqueues regex-tdfa semigroupoids split stm template-haskell text
+    time transformers unix unordered-containers vector vty
+  ];
+  executableHaskellDepends = [ base lens text vty ];
+  testHaskellDepends = [ base HUnit ];
+  homepage = "https://github.com/glguy/irc-core";
+  description = "Console IRC client";
+  license = stdenv.lib.licenses.isc;
+}
